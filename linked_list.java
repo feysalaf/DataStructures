8b4f81b4746d581905e6d21668aa44cf2fbc27e8
Faisal <faisalyusuf98@gmail.com>
2022-09-30 19:16:13 +0500

    Updates to print method

--- linked_list.java
+++ linked_list.java
@@ -40,12 +40,67 @@ public class linked_list<T> {
     public void print_list(){
         node<Integer> q = head;
         System.out.println("\nNode Address | Node Value | Node Next");
-        for(int i = 0; i<list_length; i++){
+        while(q.next != null){
             System.out.print(q);
             System.out.println("       " + q.value + "       " + q.next);
             q = q.next;
         }
+        System.out.print(q);
+        System.out.println("       " + q.value + "       " + q.next);
     }
+
+    void search(int value){
+        node<Integer> q = head;
+        System.out.println("\nSearching for: " + value + " ...");
+        while(true){
+            if(q.value == value){
+                System.out.println("Value found");
+                System.out.println(q + "       " + q.value + "       " + q.next);
+                break;
+            }
+            else{
+                if(q.next == null){
+                    System.out.println("Value not found!");
+                    break;
+                }
+                q = q.next;
+            }
+        }
+    }
+
+    void add_at_start(int value){
+        node<Integer> q = head;
+        node<Integer> p = new node<>();
+        p.value = value;
+        p.next = q;
+        head = p;
+        System.out.println("\nValue: " + value  + " added to start!");
+    }
+
+    void add_at_end(int value){
+        node<Integer> q = head;
+        while(true){
+            if(q.next == null){
+                node<Integer> p = new node<>();
+                p.value = value;
+                p.next = null;
+                q.next = p;
+                break;
+            }
+            else{
+                q = q.next;
+            }
+        }
+        System.out.println("\nValue: " + value  + " added to end!");
+    }
+
+    // boolean add_after_location(int v){
+
+    // }
+
+
+
+
 
 }
 
@@ -56,5 +111,12 @@ class test{
         linked_list<Integer> list = new linked_list<>();
         list.create_list(20);
         list.print_list();
+        list.search(212);
+        list.search(10);
+        list.add_at_start(100);
+        list.print_list();
+        list.add_at_end(200);
+        list.print_list();
+
     }
 }
